cdf1ee6fe896746cbe077e7986b7fa5baaacdbae
Alex Kite <alexkite511@gmail.com>
2019-03-17 21:09:02 -0700

    Updates

---
+++ precache-manifest.60aeb7ce844c507952521cc284400851.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e71da757eab6fddafb72",
+    "url": "/favorite-eats/static/css/main.4a91c96a.chunk.css"
+  },
+  {
+    "revision": "e71da757eab6fddafb72",
+    "url": "/favorite-eats/static/js/main.e40fd936.chunk.js"
+  },
+  {
+    "revision": "a45f5fbf9e028ceae718",
+    "url": "/favorite-eats/static/js/runtime~main.9ef1090a.js"
+  },
+  {
+    "revision": "af8e3740bd1fb5b4e361",
+    "url": "/favorite-eats/static/js/2.c598a00d.chunk.js"
+  },
+  {
+    "revision": "1ee4c3ee10a89d4ebe3782b2ff0d9eea",
+    "url": "/favorite-eats/static/media/right-arrow.1ee4c3ee.svg"
+  },
+  {
+    "revision": "79f12265afb2bdc2e44222d166a7351b",
+    "url": "/favorite-eats/static/media/LittleStar.79f12265.jpg"
+  },
+  {
+    "revision": "0eb2a70f7d567d6f5aa48d2cabbb03b6",
+    "url": "/favorite-eats/static/media/pizzalogo3.0eb2a70f.svg"
+  },
+  {
+    "revision": "a2b0cd2ea06642076d3258b0ec1970d4",
+    "url": "/favorite-eats/static/media/oasisgrillimage.a2b0cd2e.jpg"
+  },
+  {
+    "revision": "ef99412fe3a40352db2fabc950be5035",
+    "url": "/favorite-eats/static/media/skewer.ef99412f.svg"
+  },
+  {
+    "revision": "5b439f021a8ac89387228e8e93b3abc8",
+    "url": "/favorite-eats/static/media/umamiburgers.5b439f02.jpg"
+  },
+  {
+    "revision": "172f6dfb569cdf9dac38708f57d70213",
+    "url": "/favorite-eats/static/media/burger.172f6dfb.svg"
+  },
+  {
+    "revision": "4796b480c56a59cb550df6244997ac90",
+    "url": "/favorite-eats/index.html"
+  }
+];
